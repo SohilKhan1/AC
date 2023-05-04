1907720c8a9b56b1cd2eea5eefd0c8663387ea49
Sohil khan <71373974+Sohilkhan101@users.noreply.github.com>
2023-05-04 16:42:47 +0530

    Exception Handling

---
+++ Lecture17/Try_Catch_Demo1.java
@@ -0,0 +1,35 @@
+package Lecture17;
+
+public class Try_Catch_Demo1 {
+	public static void main(String[] args) {
+
+		int[] arr = new int[5];
+//		int a = 10;
+//		String s = null;
+
+		try {
+//			int k = a / 0;
+			arr[7] = 13;
+//			System.out.println(s.length());
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Finally");
+//			try {
+//				arr[7] = 13;
+//				System.out.println(s.length());
+//			} catch (ArithmeticException e) {
+//				e.printStackTrace();
+//			} catch (NullPointerException e) {
+//				e.printStackTrace();
+//			} 
+		}
+	}
+
+}
